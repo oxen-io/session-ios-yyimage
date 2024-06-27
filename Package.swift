8f5202ce53a3a4af14a820a3bed61762891290cf
Morgan Pretty <morgan.t.pretty@gmail.com>
2024-06-27 14:52:54 +1000

    Simplified and fixed the Package.swift file

--- Package.swift
+++ Package.swift
@@ -5,53 +5,26 @@ let package = Package(
     name: "YYImage",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v12)
     ],
     products: [
         .library(
             name: "YYImage",
-            targets: ["YYImageCore", "YYImageWebP", "YYImageLibwebp"]
-        )
+            targets: ["YYImage"]
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/SDWebImage/libwebp-Xcode", from: "1.3.2")
     ],
     targets: [
         .target(
-            name: "YYImageCore",
-            path: "YYImage",
-            publicHeadersPath: ".",
-            sources: ["*.h", "*.m"],
-            cSettings: [
-                .headerSearchPath("$(SRCROOT)/libwebp/src")
+            name: "YYImage",
+            dependencies: [
+                .product(name: "libwebp", package: "libwebp-Xcode")
             ],
-            linkerSettings: [
-                .linkedLibrary("z"),
-                .linkedFramework("UIKit"),
-                .linkedFramework("CoreFoundation"),
-                .linkedFramework("QuartzCore"),
-                .linkedFramework("AssetsLibrary"),
-                .linkedFramework("ImageIO"),
-                .linkedFramework("Accelerate"),
-                .linkedFramework("CoreServices/CoreServices")
-            ]
-        ),
-        .target(
-            name: "YYImageWebP",
-            dependencies: ["YYImageCore"],
-            path: "Vendor",
-            publicHeadersPath: ".",
-            sources: ["WebP.framework"]
-        ),
-        .target(
-            name: "YYImageLibwebp",
-            dependencies: ["YYImageCore", "libwebp"],
-            path: "libwebp",
-            publicHeadersPath: ".",
-            sources: ["src/*.h", "src/*.c"],
-            cSettings: [
-                .headerSearchPath("$(SRCROOT)/libwebp/src")
-            ]
+            path: ".",
+            sources: ["YYImage"],
+            publicHeadersPath: "YYImage"
         )
     ]
 )
